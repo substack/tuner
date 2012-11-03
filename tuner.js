06ee628d34af5a0edde5c161d1b5224571cf8a95
James Halliday <mail@substack.net>
2012-11-03 16:45:20 -0700

    search

--- tuner.js
+++ tuner.js
@@ -8,20 +8,38 @@ if (cmd === 'play') {
 else if (cmd === 'list') {
     list();
 }
+else if (cmd === 'search') {
+    search(process.argv.slice(3));
+}
 else if (stations[cmd]) {
     play(stations[cmd]);
 }
 else list();
 
+function search (terms) {
+    var res = terms.map(function (t) { return new RegExp(t, 'i') });
+    function match (name) {
+        var s = name + ' ' + JSON.stringify(stations[name]);
+        return res.every(function (re) { return re.test(s) });
+    }
+    
+    Object.keys(stations).sort()
+        .filter(match)
+        .forEach(function (name) {
+            console.log(show(name));
+        })
+    ;
+}
+
 function list () {
     Object.keys(stations).sort().forEach(function (name) {
-        var s = stations[name];
-        console.log(
-            name + ' - '
-            + s.description
-            + ' (' + s.location + ')'
-        );
+        console.log(show(name));
     });
+}
+
+function show (name) {
+    var s = stations[name];
+    return name + ' - ' + s.description + ' (' + s.location + ')';
 }
 
 function play (station) {
